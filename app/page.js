25ad465ff7408fc63966ec49f030ad0ff9d1d498
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2023-08-28 23:00:53 +0530

    font issue fixed

--- app/page.js
+++ app/page.js
@@ -67,20 +67,6 @@ const Index = () => {
   }
   return (
     <>
-      <Head>
-        <title>Engineer&apos;s Way | Home </title>
-        <meta charSet="UTF-8" />
-        <meta
-          name="description"
-          content="A platform for Engineering students where they can see internal construction of each and every machine how they are made (i.e induction motor, transformer, Bluetooth headphone) and CS students can code and see the animation of the code working live."
-        />
-        <meta name="keywords" content="Engineering,3D,Programming,C,Motor" />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9414302164408629"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
       <div className="w-full h-full m-0 p-0 overflow-x-hidden">
         <Background />
         <div className="overflow-hidden">
